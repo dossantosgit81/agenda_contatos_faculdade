73d96ffa3ea6e188917138ca30e8846a960c1a46
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2022-08-23 07:32:17 -0300

    create inserir contatos and atualizar

--- agenda.py
+++ agenda.py
@@ -3,37 +3,55 @@ contatos = {}
 msg_contato_inexistente = "Verfique se o nome que você digitou realmente existe."
 
 def inserir():
-    nome_contato = str(input("Insira o nome do contato aqui: ")).capitalize() 
+    nome_contato = str(input("Insira o nome do contato: ")).capitalize() 
     contatos[nome_contato] = {}
-    contatos[nome_contato]['telefone'] = str(input("Insira o telefone aqui: "))
-    contatos[nome_contato]['email'] = str(input("Insira o email aqui: "))
-    contatos[nome_contato]['midias_sociais'] = [{'twitter': str(input("Insira seu twitter aqui:  "))}]
-    contatos[nome_contato]['midias_sociais'].append({'instagram': str(input("Insira seu instagram aqui:  "))})
+    contatos[nome_contato]['telefone'] = str(input("Insira o telefone: "))
+    contatos[nome_contato]['email'] = str(input("Insira o email: "))
+    contatos[nome_contato]['midias_sociais'] = [{'twitter': str(input("Insira o login do twitter: "))}]
+    contatos[nome_contato]['midias_sociais'].append({'instagram': str(input("Insira o login instagram: "))})
     print("Contato inserido com sucesso!")
 
 def remover():
     try:
-        del contatos[str(input("Insira o nome do contato que deseja remover." )).capitalize()]
+        del contatos[str(input("Insira o nome do contato que deseja remover: " )).capitalize()]
         print("Contato removido com sucesso!!!")
     except:
         print(msg_contato_inexistente)
 
 def consultar():
     try:
-        print(contatos[str(input("Insira o nome do contato que deseja encontrar." )).capitalize()])
+        print(contatos[str(input("Insira o nome do contato que deseja encontrar: " )).capitalize()])
     except:
         print(msg_contato_inexistente)
 
-def atualizar():
-   # try:
+def atualizar(): 
+        nome_contato_antigo = str(input("Insira o nome do contato que deseja atualizar. " )).capitalize()
+        nome_contato_novo = str(input("Insira o novo nome." )).capitalize()
+
+        if nome_contato_novo == "" and nome_contato_antigo != "":
+            nome_contato_novo = nome_contato_antigo
         
-        nome_contato_atualizar = str(input("Insira o nome do contato que deseja remover." )).capitalize()
-        contatos[nome_contato_atualizar] = contatos[nome_contato_atualizar]
-       # contatos[]
-    #:
+        contatos[nome_contato_novo] = contatos.pop(nome_contato_antigo)
+        contatos[nome_contato_novo]['telefone'] = str(input("Insira o novo telefone: "))
+        contatos[nome_contato_novo]['email'] = str(input("Insira o novo email: "))
+        contatos[nome_contato_novo]['midias_sociais'] = [{'twitter': str(input("Insira o novo login do twitter: "))}]
+        contatos[nome_contato_novo]['midias_sociais'].append({'instagram': str(input("Insira o novo login instagram: "))})
+
+def inserir_contatos():
+    try:
+        qtd_usuarios = int(str(input("Quantos usuários deseja inserir? " )))
+        contador = 0;
+        while qtd_usuarios > contador:
+            inserir()
+            contador+=1
+
+    except:
+        print("Não é possivel converter o(s) carctere(s) que você digitou para número. ")
+   
 
     
 inserir()
 consultar()
+atualizar()
 print(contatos)
 
